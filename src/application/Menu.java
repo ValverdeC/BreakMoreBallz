888f6a2ee5dcde30a80768a90eeb021047df532f
ValverdeC <corentin.valverde@gmail.com>
2017-10-16 17:02:07 +0200

    feat: test css

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -7,14 +7,17 @@ import javafx.scene.shape.Rectangle;
 public class Menu extends Parent {
 	
 	public Menu() {
-		this.getStyleClass().add("menu");
-		
 		Rectangle fond_menu = new Rectangle();
+		
+		this.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+		fond_menu.setId("menu");
+		
         fond_menu.setWidth(600);
         fond_menu.setHeight(300);
         fond_menu.setArcWidth(20);
         fond_menu.setArcHeight(20);
-        fond_menu.setFill(Color.WHITE);
+        //fond_menu.setFill(Color.WHITE);
         
         this.setTranslateX(100);
         this.setTranslateY(150);
