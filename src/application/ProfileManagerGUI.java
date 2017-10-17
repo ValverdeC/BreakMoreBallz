d6a0ea004dc11d62023c65a6d58f4542aa015e3f
Baptiste <girard.baptiste@hotmail.fr>
2017-10-17 16:16:43 +0200

    ProfileManager GUI

--- src/application/ProfileManagerGUI.java
+++ src/application/ProfileManagerGUI.java
@@ -1,7 +1,11 @@
 package application;
 
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -11,25 +15,21 @@ public class ProfileManagerGUI extends Parent {
 		Rectangle fond_profile_manager = new Rectangle();
 		
 //		this.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//		Rectangle fond_menu = new Rectangle();
 		
-		fond_profile_manager.setId("profileManagerGUI");
-		
-		fond_profile_manager.setWidth(600);
-		fond_profile_manager.setHeight(300);
-		fond_profile_manager.setArcWidth(20);
-        fond_profile_manager.setArcHeight(20);
+		fond_profile_manager.getStyleClass().add("menu");
+        
+		fond_profile_manager.setWidth(410);
+		fond_profile_manager.setHeight(710);
         //fond_menu.setFill(Color.WHITE)
         
-        this.setTranslateX(100);
-        this.setTranslateY(150);
         this.getChildren().add(fond_profile_manager);
-        
         
         Button btn = new Button();
         
         btn.setLayoutX(100);
         btn.setLayoutY(80);
-        btn.setText("Hello World");
+        btn.setText("Add new profile");
         this.getChildren().add(btn);
 	}
 }
