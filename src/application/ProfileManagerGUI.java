b94bad4c40fc67932cf01a2da202b8a0ab7fa581
ValverdeC <corentin.valverde@gmail.com>
2017-10-17 16:47:14 +0200

    feat implémentation menu et profiles

--- src/application/ProfileManagerGUI.java
+++ src/application/ProfileManagerGUI.java
@@ -8,28 +8,41 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 public class ProfileManagerGUI extends Parent {
 
-	public ProfileManagerGUI() {
+	public ProfileManagerGUI(Stage stage) {
 		Rectangle fond_profile_manager = new Rectangle();
-		
-//		this.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//		Rectangle fond_menu = new Rectangle();
 		
 		fond_profile_manager.getStyleClass().add("menu");
         
 		fond_profile_manager.setWidth(410);
 		fond_profile_manager.setHeight(710);
-        //fond_menu.setFill(Color.WHITE)
         
         this.getChildren().add(fond_profile_manager);
         
         Button btn = new Button();
+        Button backBtn = new Button("Retour");
         
         btn.setLayoutX(100);
         btn.setLayoutY(80);
         btn.setText("Add new profile");
         this.getChildren().add(btn);
+        this.getChildren().add(backBtn);
+        
+        backBtn.addEventHandler(MouseEvent.MOUSE_CLICKED,
+	        new EventHandler<MouseEvent>() {
+	          @Override
+	          public void handle(MouseEvent e) {
+	        	  BorderPane root = new BorderPane();
+	        	  Scene scene = new Scene(root, 400, 700);
+	        	  scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	        	  Menu menu = new Menu(stage);
+	        	  root.getChildren().add(menu);
+	        	  
+	        	  stage.setScene(scene);
+	          }
+        });
 	}
 }
