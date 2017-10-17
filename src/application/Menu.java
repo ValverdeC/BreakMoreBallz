5ceae4ecabedd9c1e2f0103bbf537f0fc43d5837
ValverdeC <corentin.valverde@gmail.com>
2017-10-17 17:28:39 +0200

    zoeiciernoigvnziocgioayh

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -4,6 +4,8 @@ import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.Rectangle;
@@ -13,22 +15,28 @@ public class Menu extends Parent {
 	
 	public Menu(Stage stage) {
 		Rectangle fond_menu = new Rectangle();
+		Image image = new Image(getClass().getResourceAsStream("background.jpg"));
+		 
+        // simple displays ImageView the image as is
+        ImageView iv1 = new ImageView();
+        iv1.setImage(image);
 				
 		fond_menu.getStyleClass().add("menu");
 		
         fond_menu.setWidth(410);
         fond_menu.setHeight(710);
         
-        Button playButton = new Button("Jouer");
-        Button profileButton = new Button("Profiles");
+        Button playButton = new Button("JOUER");
+        Button profileButton = new Button("PROFILES");
         
         playButton.setTranslateX(180);
-        playButton.setTranslateY(330);
+        playButton.setTranslateY(550);
         
-        profileButton.setTranslateX(175);
-        profileButton.setTranslateY(370);
+        profileButton.setTranslateX(172);
+        profileButton.setTranslateY(590);
         
         this.getChildren().add(fond_menu);
+        this.getChildren().add(iv1);
         this.getChildren().add(playButton);
         this.getChildren().add(profileButton);
         
