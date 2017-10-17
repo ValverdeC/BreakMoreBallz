cd5a2ace5dc98fdd92873b61338397437314bf70
ValverdeC <corentin.valverde@gmail.com>
2017-10-17 16:47:14 +0200

    feat implémentation menu et profiles

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -4,8 +4,6 @@ import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.Rectangle;
@@ -21,24 +19,29 @@ public class Menu extends Parent {
         fond_menu.setWidth(410);
         fond_menu.setHeight(710);
         
-        Button button = new Button("Jouer");
+        Button playButton = new Button("Jouer");
+        Button profileButton = new Button("Profiles");
         
-        button.setTranslateX(180);
-        button.setTranslateY(330);
+        playButton.setTranslateX(180);
+        playButton.setTranslateY(330);
+        
+        profileButton.setTranslateX(175);
+        profileButton.setTranslateY(370);
         
         this.getChildren().add(fond_menu);
-        this.getChildren().add(button);
+        this.getChildren().add(playButton);
+        this.getChildren().add(profileButton);
         
-        button.addEventHandler(MouseEvent.MOUSE_CLICKED,
+        profileButton.addEventHandler(MouseEvent.MOUSE_CLICKED,
 	        new EventHandler<MouseEvent>() {
 	          @Override
 	          public void handle(MouseEvent e) {
-	        	  /*BorderPane root = new BorderPane();
+	        	  BorderPane root = new BorderPane();
 	        	  Scene scene = new Scene(root, 400, 700);
-	        	  Menu2 menu2 = new Menu2();
-	        	  root.getChildren().add(menu2);
+	        	  ProfileManagerGUI profileManager = new ProfileManagerGUI(stage);
+	        	  root.getChildren().add(profileManager);
 	        	  
-	        	  stage.setScene(scene);*/
+	        	  stage.setScene(scene);
 	          }
         });        
 	}
