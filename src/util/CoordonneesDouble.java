ca387e1d7847d3cebf741312f6979a320f8c0b55
chevalleader <yassin.duffoug@gmail.com>
2017-12-11 15:01:31 +0100

    Ajout de coordonnées de type Double

--- src/util/CoordonneesDouble.java
+++ src/util/CoordonneesDouble.java
@@ -1,5 +1,34 @@
 package util;
 
 public class CoordonneesDouble {
-
+		// attributs
+		private double x;
+		private double y;
+		
+		// Methodes
+		public double getX() {
+			return x;
+		}
+		public void setX(double x) {
+			this.x = x;
+		}
+		public double getY() {
+			return y;
+		}
+		public void setY(double y) {
+			this.y = y;
+		}
+		public CoordonneesDouble(double x, double y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+		
+		/* (non-Javadoc)
+		 * @see java.lang.Object#toString()
+		 */
+		@Override
+		public String toString() {
+			return "[x=" + x + ", y=" + y + "]";
+		}
 }
