095deff5f8d8b1d8084c8aeb792b4f2bf3ced083
Unknown <girard.baptiste@hotmail.fr>
2018-01-10 08:59:15 +0100

    Add High Score
    Now high score are displayed in the profil management page

--- src/metier/Profil.java
+++ src/metier/Profil.java
@@ -5,10 +5,27 @@ public class Profil {
 	private String pseudo;
 	private String optionsPath;
 	private int id;
+	private int highScore;
 	
+	//Create a new profile
 	public Profil(int id, String pseudo) {
 		this.id = id;
 		this.pseudo = pseudo;
+		this.highScore = 0;
+	}
+	
+	public Profil(int id, String pseudo, int highScore) {
+		this.id = id;
+		this.pseudo = pseudo;
+		this.highScore = highScore;
+	}
+
+	public int getHighScore() {
+		return highScore;
+	}
+
+	public void setHighScore(int highScore) {
+		this.highScore = highScore;
 	}
 
 	public String getPseudo() {
@@ -37,7 +54,7 @@ public class Profil {
 
 	@Override
 	public String toString() {
-		return id+","+pseudo+"\n";
+		return id+","+pseudo+","+highScore+"\n";
 	}
 
 }
